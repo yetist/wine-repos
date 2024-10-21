684bb1ab1e63f3c07b924680d285494905914aa2
Xiaotian Wu <yetist@gmail.com>
2024-10-21 15:45:14 +0800

    update

--- quartz/wine.lua
+++ quartz/wine.lua
@@ -6,7 +6,7 @@ publisher = 'Microsoft'
 
 url = 'https://www.51dzt.com/rubik-ssr/51dzt'
 
-depends = {'win7sp1'}
+depends = {'win7sp1-runtime'}
 source = {'override.reg'}
 
 -- install pkg into wine prefix
@@ -14,30 +14,23 @@ function install()
     print('install quartz')
     print('wine_arch=' .. wb.var.arch)
 
---  os.execute('cabextract -L -F i386/riched20.dl_ '.. wb.var.cache_dir ..'/W2KSP4_EN.EXE')
---  os.execute('cabextract --directory='.. wb.var.system32_dlls ..' i386/riched20.dl_')
-  wb.wine("regedit override.reg")
+    file = 'x86_microsoft-windows-directshow-core_31bf3856ad364e35_6.1.7601.17514_none_a877a1cc4c284497/quartz.dll'
+    os.execute('cabextract -L -F '.. file ..' '.. wb.var.cache_dir ..'/windows6.1-KB976932-X86.exe')
+    os.execute('cp x86_microsoft-windows-directshow-core_31bf3856ad364e35_6.1.7601.17514_none_a877a1cc4c284497/quartz.dll '.. wb.var.system32_dlls)
+    wb.wine(wb.var.system32_dlls .."/regsvr32.exe quartz.dll")
 
----[[
-    w_try_cabextract -d "${W_TMP}" -L -F "${filename}" "${W_CACHE}"/win7sp1/windows6.1-KB976932-X86.exe
-
-    helper_win7sp1 x86_microsoft-windows-directshow-core_31bf3856ad364e35_6.1.7601.17514_none_a877a1cc4c284497/quartz.dll
-    w_try_cp_dll "${W_TMP}/x86_microsoft-windows-directshow-core_31bf3856ad364e35_6.1.7601.17514_none_a877a1cc4c284497/quartz.dll" "${W_SYSTEM32_DLLS}/quartz.dll"
-    w_override_dlls native,builtin quartz
-    w_try_regsvr quartz.dll
-
-    if [ "${W_ARCH}" = "win64" ]; then
-        helper_win7sp1_x64 amd64_microsoft-windows-directshow-core_31bf3856ad364e35_6.1.7601.17514_none_04963d500485b5cd/quartz.dll
-        w_try_cp_dll "${W_TMP}/amd64_microsoft-windows-directshow-core_31bf3856ad364e35_6.1.7601.17514_none_04963d500485b5cd/quartz.dll" "${W_SYSTEM64_DLLS}/quartz.dll"
-        w_try_regsvr64 quartz.dll
-    fi
-]]---
-
+    if wb.var.arch == 'win64' then
+      file = 'amd64_microsoft-windows-directshow-core_31bf3856ad364e35_6.1.7601.17514_none_04963d500485b5cd/quartz.dll'
+      os.execute('cabextract -L -F '.. file ..' '.. wb.var.cache_dir ..'/windows6.1-KB976932-X64.exe')
+      os.execute('cp amd64_microsoft-windows-directshow-core_31bf3856ad364e35_6.1.7601.17514_none_04963d500485b5cd/quartz.dll '.. wb.var.system64_dlls)
+      wb.wine(wb.var.system64_dlls .."/regsvr32.exe quartz.dll")
+    end
+    wb.wine("regedit override.reg")
 end
 
 -- is this pkg installed?
 function check()
-  ret, text = wb.wine("reg QUERY \"HKEY_CURRENT_USER\\Software\\Wine\\DllOverrides\" /v msls31")
+  ret, text = wb.wine("reg QUERY \"HKEY_CURRENT_USER\\Software\\Wine\\DllOverrides\" /v quartz")
   if ret ~= 0 then
     return false
   end
