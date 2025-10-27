8d5636f3e9642ce6a945c45dae1d5d63bbdfa8ca
Xiaotian Wu <yetist@gmail.com>
2025-10-27 15:25:56 +0800

    update typeeasy

--- typeeasy/wine.lua
+++ typeeasy/wine.lua
@@ -7,8 +7,6 @@ publisher = "Kingsoft"
 url = "https://www.51dzt.com/rubik-ssr/51dzt"
 
 depends = { "fakechinese" }
--- quartz
--- riched20
 source = { "https://package.op.wpscdn.cn/wps/download/typeeasy.22063.12012.0.exe" }
 
 -- install pkg into wine prefix
@@ -18,21 +16,25 @@ end
 
 -- is this pkg installed?
 function check()
-	return (
-		wb.wine(
-			'reg QUERY "HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\酷狗音乐" /v DisplayIcon'
-		)
-	)
+	local ret, out =
+		wb.wine('reg QUERY "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\TypeEasy" /v DisplayName')
+	if ret ~= 0 then
+		return false
+	end
+
+	if string.find(wb.strip(out), "DisplayName") ~= nil then
+		return true
+	end
+	return false
 end
 
 -- run app
 function run()
-	print("hello typeeasy")
-	--  _, path = wb.wine("reg QUERY \"HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\酷狗音乐\" /v DisplayIcon")
-	--  _, path = wb.split(wb.strip(path), "\r\n", 2)
-	--  path = wb.replace(wb.strip(path), "    ", " ")
-	--  _, _, path = wb.split(path, " ", 3)
-	--  wb.wine(path)
-	-- 83067 "DisplayIcon"="C:\\Program Files (x86)\\KuGou\\KGMusic\\KuGou.exe"
-	--  w_declare_exe "$W_PROGRAMS_X86_WIN\\KuGou\KGMusic" "KuGou.exe"
+	local _, path =
+		wb.wine('reg QUERY "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\TypeEasy" /v InstallPath')
+	_, path = wb.split(wb.strip(path), "\r\n", 2)
+	path = wb.replace(wb.strip(path), "    ", " ")
+	_, _, path = wb.split(path, " ", 3)
+	path = wb.replace(wb.strip(path), "\\", "\\\\")
+	wb.wine(path .. "\\\\TypeEasy.exe")
 end
