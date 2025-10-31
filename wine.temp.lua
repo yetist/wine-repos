2efa3b2ab181f89165a50316c8e102ee3794439c
Wu Xiaotian <yetist@gmail.com>
2025-10-31 21:07:29 +0800

    update

--- wine.temp.lua
+++ wine.temp.lua
@@ -23,3 +23,34 @@ function run()
 		wb.regvalue("HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Software", "DisplayIcon")
 	wb.exec("wine", path .. "\\software.exe")
 end
+--[[
+    wb.var.prefix
+    wb.var.cache_dir
+    wb.var.build_dir
+    wb.var.arch
+    wb.var.windir
+    wb.var.program_files
+    wb.var.program_files_x86
+--    wb.var.program_files_w6432
+    wb.var.system32_dlls
+    wb.var.system64_dlls
+    wb.var.wine_commonfiles
+    wb.var.wine_commonfiles_x86
+    wb.var.version
+    wb.var.windows_dir
+    wb.var.fonts_dir
+
+    wb.pwd()
+    wb.chdir()
+    wb.dirname()
+    wb.basename()
+    -- wb.strip()
+    -- wb.split()
+    -- wb.replace()
+    wb.exists()
+    wb.isdir()
+    wb.wine()
+    wb.exec()
+    wb.regvalue()
+    wb.override_dll()
+--]]
