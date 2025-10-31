88ad81e713d04c4f3e486fc2abe9e3680fa89521
Wu Xiaotian <yetist@gmail.com>
2025-10-31 20:37:23 +0800

    update

--- wine.temp.lua
+++ wine.temp.lua
@@ -3,35 +3,23 @@ pkgver = ""
 pkgrel = 1
 pkgdesc = ""
 publisher = ""
-
-url = "http://www..com/"
-
+url = "http://www.abc.com/"
 depends = { "fakechinese" }
-
 source = { "" }
-
---  file1="THS_9.20.71_20231127.exe"
---  installed_exe1="$W_PROGRAMS_X86_WIN/同花顺/hexin.exe"
 
 -- install pkg into wine prefix
 function install()
-	print("install(" .. pkgname .. ")")
 	wb.wine(wb.basename(source[1]))
 end
 
 -- is this pkg installed?
 function check()
-	print("check(" .. pkgname .. ")")
-	--	ret = wb.wine(
-	--		'reg QUERY "HKEY_LOCAL_MACHINE\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\" /v DisplayIcon'
-	--	)
-	--return (ret == 0)
-	return false
+	return wb.regvalue("HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Software", "DisplayIcon")
 end
 
 -- run app
 function run()
-	print("run(" .. pkgname .. ")")
-	os.execute("pwd")
-	-- w_declare_exe "$W_PROGRAMS_X86_WIN\\同花顺" "hexin.exe"
+	local _, path =
+		wb.regvalue("HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Software", "DisplayIcon")
+	wb.exec("wine", path .. "\\software.exe")
 end
