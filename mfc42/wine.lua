f01c25cc9095c46f0ffd5ef02ce569d4c08c9517
Xiaotian Wu <yetist@gmail.com>
2025-11-03 18:07:56 +0800

    update

---
+++ mfc42/wine.lua
@@ -0,0 +1,20 @@
+pkgname = "mfc42"
+pkgver = "2000"
+pkgrel = 1
+pkgdesc = "Visual C++ 6 SP4 mfc42 library; part of vcrun6"
+publisher = "Microsoft"
+
+requires = { "cabextract" }
+source = { "https://download.microsoft.com/download/vc60pro/Update/2/W9XNT4/EN-US/VC6RedistSetup_deu.exe" }
+
+-- install pkg into wine prefix
+function install()
+	wb.exec("wine", "VC6RedistSetup_deu.exe", "/T:c:\\windows\\temp", "/c", "/q")
+	wb.exec("mv", wb.var.prefix .. "/drive_c/windows/temp/vcredist.exe", ".")
+	wb.exec("cabextract", "-d", wb.var.system32_dlls, "-L", "-F", "mfc42*.dll", "vcredist.exe")
+end
+
+-- is this pkg installed?
+function check()
+	return wb.exists(wb.var.system32_dlls .. "/mfc42u.dll")
+end
---
+++ vb6run/wine.lua
@@ -0,0 +1,32 @@
+pkgname = "vb6run"
+pkgver = "2004"
+pkgrel = 1
+pkgdesc = "MS Visual Basic 6 runtime sp6"
+publisher = "Microsoft"
+
+source = {
+	"https://github.com/KieuManh366377/Microsoft-Visual-Basic-6.0-Service-Pack-6/raw/refs/heads/main/VB6.0-KB290887-X86.exe",
+}
+
+-- install pkg into wine prefix
+function install()
+	wb.exec("wine", "VB6.0-KB290887-X86.exe", "/T:c:\\windows\\temp", "/c", "/q")
+	wb.exec("mv", wb.var.prefix .. "/drive_c/windows/temp/vbrun60sp6.exe", ".")
+
+	wb.exec("cabextract", "-L", "vbrun60sp6.exe")
+	wb.exec(
+		"mv",
+		"asycfilt.dll",
+		"comcat.dll",
+		"msvbvm60.dll",
+		"oleaut32.dll",
+		"olepro32.dll",
+		"stdole2.tlb",
+		wb.var.system32_dlls
+	)
+end
+
+-- is this pkg installed?
+function check()
+	return wb.exists(wb.var.system32_dlls .. "/msvbvm60.dll")
+end
