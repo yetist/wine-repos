e0e18c91b42ec3ec1be848b641939c766cc52db7
Xiaotian Wu <yetist@gmail.com>
2025-11-04 10:52:55 +0800

    update

--- aardio/wine.lua
+++ aardio/wine.lua
@@ -5,21 +5,21 @@ pkgdesc = "桌面软件快速开发工具"
 publisher = "aardio"
 url = "https://www.aardio.com/"
 icon = "https://www.aardio.com/images/favicon32.ico"
-requires = { "7z", "wrestool" }
+requires = { "7z" }
 depends = { "fakechinese" }
 source = { "https://d.aardio.com/ide/aardio.7z" }
 
 function install()
-	wb.exec("mkdir", "-p", wb.var.program_files .. "/aardio")
-	wb.exec("7z", "x", "-o" .. wb.var.program_files .. "/aardio", "aardio.7z")
+	wb.exec("mkdir", "-p", wb.var.program_files_x86 .. "/aardio")
+	wb.exec("7z", "x", "-o" .. wb.var.program_files_x86 .. "/aardio", "aardio.7z")
 
-	wb.desktop("aardio", "Development;", wb.var.program_files .. "/aardio/aardio.exe", 128)
+	wb.desktop("aardio", "Development;", wb.var.program_files_x86 .. "/aardio/aardio.exe", 128)
 end
 
 function check()
-	return wb.exists(wb.var.program_files .. "/aardio/aardio.exe")
+	return wb.exists(wb.var.program_files_x86 .. "/aardio/aardio.exe")
 end
 
 function run()
-	wb.exec("wine", wb.var.program_files .. "/aardio/aardio.exe")
+	wb.exec("wine", wb.var.program_files_x86 .. "/aardio/aardio.exe")
 end
